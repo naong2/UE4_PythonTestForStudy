929ba5ba9ea698e1546b24d85d62467add4bdf29
SUNGJEA PARK <naong2@gmail.com>
2019-01-14 17:23:30 +0900

    PySide 샘플 올림

---
+++ Content/Scripts/Naong2/Sample_Pyside01.py
@@ -0,0 +1,41 @@
+﻿import ueqt
+from PySide2 import QtCore, QtWidgets, QtGui
+import unreal_engine as ue
+
+from unreal_engine import FARFilter
+
+_filter = FARFilter()
+_filter.class_names = ['SkeletalMesh']
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.vertical = QtWidgets.QVBoxLayout()
+        self.scroll = QtWidgets.QScrollArea()
+        self.content = QtWidgets.QWidget()
+        self.scroll.setWidget(self.content)
+        self.scroll.setWidgetResizable(True)
+        self.layout = QtWidgets.QVBoxLayout()
+
+        for asset_data in ue.get_assets_by_filter(_filter, True):
+            try:
+                thumbnail = asset_data.get_thumbnail()
+            except:
+                break
+            label = QtWidgets.QLabel()
+            data = thumbnail.get_uncompressed_image_data()
+            image = QtGui.QImage(data, 256, 256, QtGui.QImage.Format_RGB32)
+            label.setPixmap(QtGui.QPixmap.fromImage(image).scaled(256, 256))
+            self.layout.addWidget(label)
+
+        self.content.setLayout(self.layout)
+        self.vertical.addWidget(self.scroll)
+        self.setLayout(self.vertical)
+
+widget = MyWidget()
+widget.resize(800, 600)
+widget.show()
+
+root_window = ue.get_editor_window()
+
+root_window.set_as_owner(widget.winId())
---
+++ Content/Scripts/PythonScript3.py
@@ -0,0 +1,5 @@
+import ueqt
+import Naong2
+#ue.log('hello')
+
+Naong2.play()
